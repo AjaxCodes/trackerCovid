677e0c24d16cfd7e19bd533eef3377885c4bda65
AjaxCodes <ajaxcodes@Ajax.mackbook>
2021-02-23 15:43:11 -0600

    country dropdown working , finished

--- covid-tracker/src/Components/Header.js
+++ covid-tracker/src/Components/Header.js
@@ -3,19 +3,24 @@ import { FormControl, Select, MenuItem } from "@material-ui/core";
 import "./Header.css";
 
 function Header() {
-  const [countries, setCountries] = useState(["USA", "UK", "France"]);
+  const [countries, setCountries] = useState([]);
 
-
-  //https://disease.sh/v3/covid-19/countries
   //https://disease.sh/v3/covid-19/states/{states}
 
-
   useEffect(() => {
-      
-
-
-
-  }, [countries]);
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }));
+          setCountries(countries);
+        });
+    };
+    getCountriesData();
+  }, []);
   return (
     <div className="header">
       <div className="appHeader">
@@ -24,7 +29,7 @@ function Header() {
         <FormControl className="appDropdown">
           <Select varient="outlined" vlaue="abc">
             {countries.map((country) => (
-              <MenuItem value={country}>{country}</MenuItem>
+              <MenuItem value={country.value}>{country.name}</MenuItem>
             ))}
           </Select>
         </FormControl>
